6efcf3b412c396eac0869e978619be3a477160bf
MASUR89 <surmava9@gmail.com>
2025-12-10 21:03:14 +0400

    Formik

--- glass/src/Components/UseMemo.tsx
+++ glass/src/Components/UseMemo.tsx
@@ -1,35 +1,54 @@
-import { useForm } from "react-hook-form";
+import { Formik, Form, Field } from "formik"
 
 const UseMemo = () => {
 
-  const {register, handleSubmit, formState: {errors} } = useForm();
+//   const {register, handleSubmit, formState: {errors} } = useForm();
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-  }
+//   const onSubmit = (data: any) => {
+//     console.log(data);
+//   }
+
+//   return (
+//      <div>
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <input {...register("name",{required:"Name is required", minLength: {value: 12, message: "Name must be more than 2 characters"}})} placeholder='Username' />
+//         {errors.name && <p>{errors.name.message as string}</p>}
+//         <input {...register("email",{required:"Email is required"})} placeholder='Email' />
+//         {errors.email && <p>{errors.email.message as string}</p>}
+//         <input {...register("age",{required:"Indicate your age"})} placeholder='Your age' />
+//         {errors.age && <p>{errors.age.message as string}</p>}
+//         <input {...register("ID",{required:"Your ID number"})} placeholder='ID number' />
+//         {errors.id && <p>{errors.id.message as string}</p>}
+//         <input {...register("password",{required:"Password is required"})} placeholder='Password' />
+//         {errors.password && <p>{errors.password.message as string}</p>}
+//         <input {...register("confirmPassword",{required:"Confirm Password is required"})} placeholder='Confirm Password' />
+//         {errors.confirmPassword && <p>{errors.confirmPassword.message as string}</p>}
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+
 
   return (
-     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("username",{required:"Username is required", minLength: {value: 10, message: "Username must be more than 10 characters"}})} placeholder='Username' />
-        {errors.username && <p>{errors.username.message as string}</p>}
-        <input {...register("email",{required:"Email is required"})} placeholder='Email' />
-        {errors.email && <p>{errors.email.message as string}</p>}
-        <input {...register("password",{required:"Password is required"})} placeholder='Password' />
-        {errors.password && <p>{errors.password.message as string}</p>}
-        <input {...register("confirmPassword",{required:"Confirm Password is required"})} placeholder='Confirm Password' />
-        {errors.confirmPassword && <p>{errors.confirmPassword.message as string}</p>}
-        <button>Submit</button>
-      </form>
-    </div>
-  )
-}
+     <>
+     <Formik
+      initialValues={{ userame: 'maia' }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+     >
+      <Form>
+        <Field name="username" placeholder="user"/>
+      </Form>
+     </Formik>
+     </>
+    )
+  }
 
 export default UseMemo
 
-//subject: useCallback, useMemo
-//goal: understand useMemo and useCallback
 
 //new subject: formik vs react-hook-form
-//new goal: learn the differences between formik and react-hook-form and when to use each one
 
